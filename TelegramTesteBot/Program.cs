a7f3ce0a4e444ed8e3d5800bb4cd0f27f2d5b719
Iann Leles <iannleles@outlook.com>
2021-10-31 22:05:29 -0300

    criando projeto console para teste do telegram

---
+++ TelegramTesteBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegramTesteBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
