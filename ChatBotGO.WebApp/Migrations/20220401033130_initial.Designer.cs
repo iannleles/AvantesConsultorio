7333e92ef40d865b37bd76e2c0b4f08caea86d19
Iann Leles <iann.marcos97@gmail.com>
2022-04-01 00:36:00 -0300

    connect string

--- ChatBotGO.WebApp/Migrations/20220401033130_initial.Designer.cs
+++ ChatBotGO.WebApp/Migrations/20220401033130_initial.Designer.cs
@@ -3,14 +3,16 @@ using ChatBotGO.WebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ChatBotGO.WebApp.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20220401033130_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
