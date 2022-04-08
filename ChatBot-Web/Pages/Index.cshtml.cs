9aa66360758f0b9eb27e4d0dacd6bebb5e6a6638
Iann Leles <iann.marcos97@gmail.com>
2022-04-07 23:53:08 -0300

    merge

---
+++ ChatBot-Web/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot_Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
