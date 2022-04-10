46ab76c88d3a4b7ea87befa83b9ae80ec0e35304
Iann Leles <iann.marcos97@gmail.com>
2022-04-10 01:40:38 -0300

    Implementação das views e cadastros, agendamento

---
+++ ChatBot-Web/Models/Especialidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot_Web.Models
+{
+    public class Especialidade
+    {
+        public string Cardiologista { get; set; }
+        public string Nutricionista { get; set; }
+        public string Dentista { get; set; }
+        public string ClinicoGeral { get; set; }
+        public string Pediatria { get; set; }
+    }
+}
---
+++ ChatBot-Web/Models/ViewModel/CadastroUsuarioViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot_Web.Models.ViewModel
+{
+    public class CadastroUsuarioViewModel
+    {
+        public int UsuarioId { get; set; }
+
+        public int PerfilId { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public string CPF { get; set; }
+
+        public string Telefone { get; set; }
+
+    }
+}
