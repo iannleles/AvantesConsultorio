b269d028b328a7ecc736d6d056cd259ecb2495fc
Iann Leles <iann.marcos97@gmail.com>
2022-04-17 02:18:24 -0300

    inicio Agendamento Post

--- ChatBot-Web/Controllers/CadastroController.cs
+++ ChatBot-Web/Controllers/CadastroController.cs
@@ -3,6 +3,7 @@ using ChatBot_Web.Models;
 using ChatBot_Web.Models.ViewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -30,13 +31,57 @@ namespace ChatBot_Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }              
+        }
 
 
         public IActionResult Agendamento()
-        {
+        {                       
+
+            ViewData["EspecialidadeId"] = new SelectList(_context.Especialidade, "Id", "Nome");           
+            ViewData["UFSigla"] = new SelectList(_context.UF, "UFId", "Nome");           
 
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Agendamento(AgendamentoViewModel agendamentoViewModel) 
+        {
+            Agendamento agendamento = new Agendamento();
+
+            if (ModelState.IsValid)
+            {
+                //var especialidade = _context.Especialidade;
+                //ViewData["Id"] = new SelectList(especialidade, "Id", "Nome", agendamentoViewModel.EspecialidadeId);
+                agendamento.Especialidade = agendamentoViewModel.Especialidade;
+                agendamento.Data = agendamentoViewModel.Data;
+                agendamento.Horario = agendamentoViewModel.Horario;
+                agendamento.Paciente = new Paciente()
+                {
+                    Nome = agendamentoViewModel.Paciente.Nome,
+                    Sobrenome = agendamentoViewModel.Paciente.Sobrenome,
+                    RG = agendamentoViewModel.Paciente.RG,
+                    CPF = agendamentoViewModel.Paciente.CPF,
+                    DataNascimento = agendamentoViewModel.Paciente.DataNascimento,
+                    Telefone = agendamentoViewModel.Paciente.Telefone,
+                    Celular = agendamentoViewModel.Paciente.Celular,
+                };
+                agendamento.Endereco = new Endereco()
+                {
+                    Cep = agendamentoViewModel.Endereco.Cep,
+                    Logradouro = agendamentoViewModel.Endereco.Logradouro,
+                    Numero = agendamentoViewModel.Endereco.Numero,
+                    Complemento = agendamentoViewModel.Endereco.Complemento,
+                    Bairro = agendamentoViewModel.Endereco.Bairro,
+                    Cidade = agendamentoViewModel.Endereco.Cidade,
+                    UFSigla = agendamentoViewModel.Endereco.UFSigla,
+                };            
+            }
+
+            _context.Agendamento.AddAsync(agendamento);
+            _context.SaveChanges();
+
+            return View(agendamentoViewModel);
         }
 
 
---
+++ ChatBot-Web/Models/ViewModel/AgendamentoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatBot_Web.Models.ViewModel
+{
+    public class AgendamentoViewModel
+    {
+        public int EspecialidadeId { get; set; }
+
+        public virtual Especialidade Especialidade { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public DateTime Horario { get; set; }
+
+        public Paciente Paciente { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+
+    }
+}
